4659886178825e057e2f1162b2e18d7f57572ccd
Marko Radonic <marko.radonic@inform-technologies.com>
2021-10-22 21:42:43 +0200

    finished crud operation

--- jsForm/js/main.js
+++ jsForm/js/main.js
@@ -4,62 +4,79 @@ let addPersonForm = document.querySelector('#person-form');
 let titleValue = document.getElementById('name');
 let lastNameValue = document.getElementById('lastName');
 let jmbgValue = document.getElementById('jmbg');
+let btnSubmit = document.querySelector('.btn');
 let output = '';
 let url = "https://localhost:44300/api/Persons";
 
 function renderPost(person) {
     person.data.forEach(element => {
         output += `
-        <tr>
-        <td class="person-name">${element.name}</td>
-        <td>${element.lastName}</td>
-        <td>${element.jmbg}</td>
-        <td><a href="#" class=" btn btn-danger btn-sm delete" id='${element.id}' >Delete</a>
-        <a href="#" class="btn btn-primary btn-sm delete" id='edit' >Edit</a></td>
-        </tr>
+        <div >
+            <tr id=${element.id}>
+            <td class="person-name">${element.name}</td>
+            <td class="person-lastName">${element.lastName}</td>
+            <td class="person-jmbg">${element.jmbg}</td>
+            <td><a href="#" class=" btn btn-danger btn-sm delete" id="delete-person" >Delete</a>
+            <a href="#" class="btn btn-primary btn-sm delete" id='edit-person' >Edit</a></td>
+            </tr>
+        </div>
         `
         list.innerHTML = output;
     });
 }
+
 fetch(url)
     .then(res => res.json())
     .then(person => renderPost(person))
 
-list.addEventListener('click', function (e) {
-    e.preventDefault();
-    let editBtn = e.target.id == "edit";
-
-    if (editBtn) {
-
-        let titleContent = document.querySelector('.person-name').textContent;
-        titleValue.value = titleContent;
-
-    }
-})
-
-
+//DELETE and EDIT 
 list.addEventListener('click', (e) => {
     e.preventDefault();
-    let deleteButton = e.target.id;
-    let editButton = e.target.id;
+    let deleteButton = e.target.id == 'delete-person'
+    let editButton = e.target.id == 'edit-person'
+    let id = e.target.parentElement.parentElement.id;
 
 
+    // delete 
     if (deleteButton) {
-        fetch(`${url}/${deleteButton}`, {
-                method: 'DELETE',
+        fetch(`${url}/${id}`, {
+                method: "DELETE",
             })
             .then(res => res.json())
             .then(() => location.reload())
     }
+    // edit
+    if (editButton) {
+        const parent = e.target.parentElement.parentElement;
+        let nameContent = parent.querySelector('.person-name').textContent;
+        let lastName = parent.querySelector('.person-lastName').textContent;
+        let jmbg = parent.querySelector('.person-jmbg').textContent;
+
+        titleValue.value += nameContent;
+        lastNameValue.value += lastName;
+        jmbgValue.value += jmbg;
+    }
+
+    btnSubmit.addEventListener('click', (e) => {
+        e.preventDefault();
+        fetch(`${url}/${id}`, {
+                method: "PUT",
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    name: titleValue.value,
+                    lastName: lastNameValue.value,
+                    jmbg: jmbgValue.value
+                })
+
+            })
+            .then(() => location.reload())
+    })
+
 })
 
-
-
-
-
 // Create Person
-//Method: POST
-
 addPersonForm.addEventListener('submit', (e) => {
     e.preventDefault();
 
@@ -69,40 +86,12 @@ addPersonForm.addEventListener('submit', (e) => {
         jmbg: jmbgValue.value
     }
 
-    fetch("https://localhost:44300/api/Persons", {
+    fetch(`${url}`, {
         method: 'POST',
         body: JSON.stringify(_data),
         headers: new Headers({
             'Content-Type': 'application/json'
         })
 
-    });
-
-    // const dataArr = [];
-    // dataArr.push(ListOfData)
-
-    // fetch(url)
-
-    //     .then(res => res.json())
-    //     .then(data => renderPost(data.ListOfData))
-
-
-
-    window.location.reload()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    }).then(() => location.reload())
 })
--- jsForm/js/person.js
+++
@@ -1,8 +0,0 @@
-class Person {
-    constructor(name, lastName, JMBG, id) {
-        this.name = name;
-        this.lastName = lastName;
-        this.JMBG = JMBG;
-        this.id = id;
-    }
-}
